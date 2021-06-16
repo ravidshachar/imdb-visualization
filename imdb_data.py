3c3bd346bc74ebe1194264baf17fb3a20e4fdd43
ravid <ravidshachar47@gmail.com>
2021-06-16 14:39:11 +0300

    Changed imdb_data

--- imdb_data.py
+++ imdb_data.py
@@ -1,54 +1,44 @@
 from bs4 import BeautifulSoup
 from time import time,sleep
 from random import randint
-from IPython.core.display import clear_output
-from requests import get
+import requests
 import pandas as pd
 
 HEADERS = {"Accept-Language": "en-US, en;q=0.5"}
 NUMBER_OF_REQUESTS = 3
 
-def get_movie_dataframe(pages_number,years_url):
+def get_movie_dataframe(pages_number=4,years=[str(i) for i in range(2000,2018)]):
 	record_lists = []
 	pages = [str(i) for i in range(1,pages_number)]
-	
-	# Redeclaring the lists to store data in
-	names = []
-	years = []
-	imdb_ratings = []
-	metascores = []
-	votes = []
-	directors = []
-	stars = []
 
 	# Preparing the monitoring of the loop
 	start_time = time()
-	requests = 0
+	requests_num = 0
 
 	# For every year in the interval 2000-2017
-	for year_url in years_url:
+	for year in years:
 
 		# For every page in the interval 1-4
 		for page in pages:
 
 			# Make a get request
-			response = get('https://www.imdb.com/search/title?title_type=feature,tv_movie&release_date=' + year_url +
+			response = requests.get('https://www.imdb.com/search/title?title_type=feature,tv_movie&release_date=' + year +
 			'&sort=num_votes,desc&page=' + page, headers = HEADERS)
 
-			# Pause the loop
+			# Pause the loop so that imdb won't block you
 			sleep(randint(4,8))
 
 			# Monitor the requests
-			requests += 1
+			requests_num += 1
 			elapsed_time = time() - start_time
-			print('Request:{}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
+			print('Request:{}; Frequency: {} requests/s'.format(requests_num, requests_num/elapsed_time))
 
 			# Throw a warning for non-200 status codes
 			if response.status_code != 200:
-				print('Request: {}; Status code: {}'.format(requests, response.status_code))
+				print('Request: {}; Status code: {}'.format(requests_num, response.status_code))
 
 			# Break the loop if the number of requests is greater than expected
-			if requests > NUMBER_OF_REQUESTS:
+			if requests_num > NUMBER_OF_REQUESTS:
 				print('Number of requests was greater than expected.')
 				break
 			
@@ -64,42 +54,29 @@ def get_movie_dataframe(pages_number,years_url):
 
 					# Scrape the name
 					name = container.h3.a.text
-					names.append(name)
 
 					# Scrape the year
-					year = container.h3.find('span', class_ = 'lister-item-year').text
-					years.append(year)
+					movie_year = container.h3.find('span', class_ = 'lister-item-year').text
 
 					# Scrape the IMDB rating
 					imdb = float(container.strong.text)
-					imdb_ratings.append(imdb)
 
 					# Scrape the Metascore
 					m_score = container.find('span', class_ = 'metascore').text
-					metascores.append(int(m_score))
 
 					# Scrape the number of votes
 					vote = container.find('span', attrs = {'name':'nv'})['data-value']
-					votes.append(int(vote))
 					
 					director = container.find('p', class_ = '').find_all('a')[0].text
-					star = [x.text for x in container.find('p', class_ = '').find_all('a')[1:]]
+					actors = [x.text for x in container.find('p', class_ = '').find_all('a')[1:]]
 					
-					directors.append(director)
-					stars.append(star)
-					
-					record_lists.append([name,year,imdb,m_score,vote,director,star])
+					record_lists.append([name,movie_year,imdb,m_score,vote,director,actors])
 
 					#star = container.find('p')
-		if requests > NUMBER_OF_REQUESTS:
+		if requests_num > NUMBER_OF_REQUESTS:
 			break
 	df = pd.DataFrame(record_lists,columns=["name","year","imdb_rating","metascore","votes","director","actors"])
-	print(df)
 	return df
-
-def main():
-	pages_number = 5
-	years_url = [str(i) for i in range(2000,2018)]
-	get_movie_dataframe(pages_number,years_url)
 	
-main()
+if __name__ == "__main__":
+	print(get_movie_dataframe())
